2e0cd50e498c9e890accbcc1cbfec167b8cc7757
Nathalie Mayor <58463185+nathaliemayor@users.noreply.github.com>
2022-03-01 11:16:35 +0100

    Update A2_risk_measures.R

--- A2_risk_measures.R
+++ A2_risk_measures.R
@@ -24,11 +24,9 @@ setwd(dirname(current_path))
 
 data1<-read.csv("data_task1.csv",header = TRUE)
 data1rev=data1[order(nrow(data1):1),]
-data1rev
 
 data1rev$Date <- as.Date(data1rev$Date, format="%d.%m.%y")
 class(data1rev$Date)
-data1rev
 
 tickers<-c("AAPL","JPM","PFE","V")
 data1<-getSymbols(tickers,from='2009-08-30',to='2020-09-30',warnings = FALSE,auto.assign = TRUE)
@@ -46,7 +44,6 @@ AAPL<-data[,1]
 JPM<-data[,2]
 PFE<-data[,3]
 V<-data[,4]
-V
 
 w1<-sum(AAPL)/sum(data)
 w2<-sum(JPM)/sum(data)
@@ -79,27 +76,16 @@ T_VaR_JPM<-(-(meanJPM-2.33*sdJPM))
 T_VaR_PFE<-(-(meanPFE-2.33*sdPFE))
 T_VaR_V<-(-(meanV-2.33*sdV))
 
-E_VaR_AAPL
-T_VaR_AAPL
-E_VaR_JPM
-T_VaR_JPM
-E_VaR_PFE
-T_VaR_PFE
-E_VaR_V
-T_VaR_V
 
 ### b. 1 day portfolio VaR MEAN of all returns
 portfolioAJPV<-.xts(rowSums(data),.index(data))
 portfolio.DailyLogReturns<- na.omit(Return.calculate(portfolioAJPV, method = "log"))
-portfolio.DailyLogReturns
 
 E_VaR_PTF<--quantile(portfolio.DailyLogReturns,0.01)
 meanPTF<-mean(portfolio.DailyLogReturns)
 sdPTF<-sd(portfolio.DailyLogReturns)
 T_VaR_PTF<--(-meanPTF-2.33*sdPTF)
 
-E_VaR_PTF
-T_VaR_PTF
 
 ### VaR from individual assets
 CorrAJPV<-cor(AJPV.DailyLogReturns)
@@ -109,10 +95,6 @@ vs<-weights*VaRi
 vs<-as.matrix(vs)
 
 VaR_Portfolio<-(t(vs)%*%CorrAJPV%*%vs)^0.5
-
-VaR_Portfolio
-
-
 
 ### c. histogram of the portfolio returns
 hist(portfolio.DailyLogReturns*100,nclass = 100,col="purple",main = "Histogram of Portfolio daily returns",xlab = "daily returns")
@@ -131,16 +113,10 @@ VaR90i<-c(T_VaR90_AAPL,T_VaR90_JPM,T_VaR90_PFE,T_VaR90_V)
 v90<-weights*VaR90i
 v90<-as.matrix(v90)
 VaR90_Portfolio<-(t(v90)%*%CorrAJPV%*%v90)^0.5
-VaR90_Portfolio
-
-
-
-
 
 #####################################################################################################################
 ############################################### PROBLEM 2  ###########################################################
 #####################################################################################################################
-
 
 data2<-read.csv("data_task2.csv",header = TRUE)
 data2rev=data2[order(nrow(data2):1),]
@@ -149,7 +125,6 @@ data2ts<-ts(data2rev,frequency = 12,start =c(2009,09))
 data2ts<-data2ts[,-1]
 plot(data2ts)
 MonthlySPYLogReturns<-na.omit(Return.calculate(data2ts, method = "log"))
-MonthlySPYLogReturns
 plot(MonthlySPYLogReturns)
 hist(MonthlySPYLogReturns,n=50)
 
@@ -158,26 +133,21 @@ var(MonthlySPYLogReturns)
 dfMLR<-as.data.frame(MonthlySPYLogReturns)
 h1<-mean(dfMLR$x)
 SemiVariance<-sum((dfMLR[dfMLR$x<h1,])^2)/133
-SemiVariance
 
 ### b. target semi variance 
 h<-0.005
-SemiVariance2
 dfMLR[dfMLR$x<0.005,]
 SemiVariance2<-sum((dfMLR[dfMLR$x<0.005,])^2)/133
-SemiVariance2
 
 ### semi variance = target semivariance when h = mean of the returns = 0.88%
 
 ### c. Semi sd, Sharpe and sortino    
 rf<-0.001/252
 SemiSD<-sqrt(SemiVariance)
-SemiSD
 SharpeRatio<-(mean(dfMLR$x)-rf)/sd(dfMLR$x)
 SortinoRatio<-(mean(dfMLR$x)-h)/sqrt(SemiVariance2)
-SharpeRatio
 mean(dfMLR$x)
-SortinoRatio
+
 ### d. All time high
 n=133
 
@@ -194,7 +164,6 @@ ad<-as.vector(data2ts)
 peaksindex<-match(ATH,ad)
 peaksvalues<-as.data.frame(data2ts[peaksindex])
 peaksindex[1:4]
-peaksindex
 data2rev[peaksindex]
 datepeaks<-as.data.frame(data2rev[peaksindex,1])
 
@@ -206,7 +175,6 @@ for (i in 55) {
 
 maxdrawdown(data2ts[6:133])
 data2ts[124:127]
-data2ts
 
 min(data2ts[peaksindex[3]:peaksindex[4]])
 
@@ -214,8 +182,6 @@ for (i in 1:54) {
   print(min(data2ts[peaksindex[i]:peaksindex[1+i]]))
 }
 
-
-peaksindex
 
 plot(data2ts)
 
@@ -231,7 +197,6 @@ valuesmaxdrawdowns<-as.data.frame(peaksvalues-maxdrawdowns)
 plot(percentagedrawdowns)
 
 md<-read.csv("Book2.csv",header = TRUE)
-md
 mdrev=md[order(nrow(md):1),]
 md$date<-as.Date(md$date,format = "%d.%m.%Y")
 mdts<-ts(mdrev,frequency = 12,start =c(2009,09))
@@ -250,7 +215,6 @@ data3ts <- xts(subset(data3rev, select=-Date), order.by=data3rev$Date)
 SX8Pprices<-data3rev[,2]
 AHSprices<-data3rev[,3:5]
 
-
 ###a. 1'000'000 eq. weighted portolio
 AHSpricesTS<-data3ts[,3:5]
 weightsPTF3<-as.numeric(AHSpricesTS[1,])/sum(as.numeric(AHSpricesTS[1,]))
@@ -264,7 +228,6 @@ SX8Pprices<-data3rev[,2]
 SX8PpricesTS<-.xts(x = SX8Pprices, .index(data3ts))
 
 plot(SX8PpricesTS)
-
 
 ### index tracking error
 DailyLogReturnsSX8P<-na.omit(Return.calculate(SX8PpricesTS, method = "log"))
@@ -282,11 +245,7 @@ VarianceTE<-var(ReturnsDiff)
 SDTE<-IndexTrackingError
 minTE<-min(ReturnsDiff)
 maxTE<-max(ReturnsDiff)
-meanTE
-VarianceTE
-SDTE
-minTE
-maxTE
+
 ### b. rebalancing: weights proportional to market cap
 Q42019start<-data3ts[1,5:7]
 Q42019start<-as.numeric(Q42019start)
@@ -336,17 +295,11 @@ plot(returnsDiff2)
 lines(ReturnsDiff,col="4")
 
 IndexTrackingError2<-sqrt(sum(returnsDiff2^2)/255)
-IndexTrackingError2
 sd(returnsDiff2)
 
 
 ### summary statistics
 meanTE2<-mean(returnsDiff2)
-meanTE2
-VarianceTE2
-SDTE2
-minTE2
-maxTE2
 VarianceTE2<-var(returnsDiff2)
 SDTE2<-IndexTrackingError2
 minTE2<-min(returnsDiff2)
@@ -371,20 +324,6 @@ ESs95<-(-30*means+sdSX8P*sqrt(30)*dnorm(1.64)/0.05)
 ESa99<-(-30*meana+sdAHS*sqrt(30)*dnorm(2.33)/0.01)
 ESs99<-(-30*means+sdSX8P*sqrt(30)*dnorm(2.33)/0.01)
 
-weights
-sdAHS
-sdSX8P
-MADAHS
-MADSX8P
-VARAHS95
-VARS95
-VARAHS99
-VARs99
-ESa95
-ESs95
-ESa99
-ESs99
-
 maxDrawdown(DailyLogReturnsAHS)
 maxDrawdown(DailyLogReturnsSX8P)
 
@@ -392,7 +331,6 @@ maxDrawdown(DailyLogReturnsSX8P)
 
 ### d. qq plot: normality of returns (SX8P & SAP, ASML, PROSUS)
 
-DailyLogReturnsSX8P
 qqnorm(DailyLogReturnsSX8P)
 qqnormPlot(DailyLogReturnsSX8P)
 hist(DailyLogReturnsSX8P,nclass = 75)
@@ -403,7 +341,7 @@ qqnorm(DailyLogReturnsSAP)
 qqnormPlot(DailyLogReturnsSAP)
 hist(DailyLogReturnsSAP,nclass = 75)
 lines(seq(min(DailyLogReturnsSAP), max(DailyLogReturnsSAP), by = .01),dnorm(seq(min(DailyLogReturnsSAP), max(DailyLogReturnsSAP), by = .01), mean = mean(DailyLogReturnsSAP), sd = sd(DailyLogReturnsSAP)))
-DailyLogReturnsSX8P
+
 
 DailyLogReturnsASML<-na.omit(Return.calculate(data3ts[,3], method = "log"))
 qqnorm(DailyLogReturnsASML)
@@ -419,7 +357,6 @@ lines(x,y)
 
 
 x <- seq(min(DailyLogReturnsPROSUS), max(DailyLogReturnsPROSUS), by = .01)
-x
 
 # Choose the mean as 2.5 and standard deviation as 0.5.
 y <- dnorm(x, mean = mean(DailyLogReturnsPROSUS), sd = sd(DailyLogReturnsPROSUS))
